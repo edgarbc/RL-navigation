fa9ded96f758e08a0bee1eae750a24e5f480aaeb
edgarbc <edgar.bermudez@gmail.com>
2018-07-24 22:15:52 -0600

    agent have tasks and policies to carry them

--- agents/agent.py
+++ agents/agent.py
@@ -4,8 +4,16 @@ class Agent:
     # agent class
     def __init__(self):
         self.pos = [0,0]
+		self.define_policy()
+		self.color = [0, 0, 1]
+		self.reward = 0
+		self.task = 0
+
     def get_pos(self):
         # returns the current position of the agent
         return self.pos
+
     def display(self):
         pass
+	
+	
