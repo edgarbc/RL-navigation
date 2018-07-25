bb03f125d8cb287d8162bac93f639f5be460dde0
edgarbc <edgar.bermudez@gmail.com>
2018-07-24 22:16:53 -0600

    objects can be anything in the world that is not an agent. They have properties and can be formed by units (cells in the arena)

--- objects/object.py
+++ objects/object.py
@@ -1,11 +1,23 @@
 
 class Object:
-    def __init__(self, type):
-        self.type = type
-        self.pos = [0, 2]
-        self.color = [0, 0, 0]
+	# This class is for walls, lava and the goal. ERIC: maybe we should have another class for having a set of units
+    def __init__(self, type, num_units):
+    	self.type = type
+    	self.pos = [0, 2]
+    	self.color = [0, 0, 0]
+		self.num_units = num_units
+		for i in range (0, num_units):
+			self.units[i] = Unit([0,0])
 
     def display(self):
         # displays itself
-        pass
+        print('object showed')
 
+	
+
+class Unit:
+	def __init__(self):
+		self.pos = []
+		self.color = []
+		self.type = []
+	
